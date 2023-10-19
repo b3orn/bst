53a3f9690aeed4c2765e21ac8f3e66fad7ee9692
Björn Schulz <bjoern@fac3.org>
2023-10-19 21:39:22 +0200

    Update memory debugger

--- src/debug.c
+++ src/debug.c
@@ -9,7 +9,19 @@ BST_API(void*)
 bst_debug_malloc(char *filename, size_t line, size_t size) {
     uint8_t *ptr;
 
-    ptr = malloc(size);
+    if(!(ptr = malloc(size + 3 * sizeof(size_t)))) {
+        fprintf(stderr, "BST MEM DEBUG %s:%zu malloc(%zu) = NULL\n",
+                filename,
+                line,
+                size);
+
+        return NULL;
+    }
+
+    *(size_t*)ptr = size;
+
+    memset(ptr + sizeof(size_t), 0xff, sizeof(size_t));
+    memset(ptr + size + 2 * sizeof(size_t), 0xff, sizeof(size_t));
 
     fprintf(stderr, "BST MEM DEBUG %s:%zu malloc(%zu) = %p\n",
             filename,
@@ -17,7 +29,7 @@ bst_debug_malloc(char *filename, size_t line, size_t size) {
             size,
             (void*)ptr);
 
-    return (void*)ptr;
+    return (void*)(ptr + 2 * sizeof(size_t));
 }
 
 
@@ -25,14 +37,22 @@ BST_API(void*)
 bst_debug_calloc(char *filename, size_t line, size_t n, size_t size) {
     uint8_t *ptr;
 
-    ptr = calloc(n, size);
-
-    fprintf(stderr, "BST MEM DEBUG %s:%zu calloc(%zu, %zu) = %p\n",
+    fprintf(stderr, "BST MEM DEBUG %s:%zu begin calloc(%zu, %zu)\n",
             filename,
             line,
             n,
-            size,
-            (void*)ptr);
+            size);
+
+    if (!(ptr = bst_debug_malloc(filename, line, n * size)))
+        return NULL;
+
+    memset(ptr, 0, n * size);
+
+    fprintf(stderr, "BST MEM DEBUG %s:%zu end calloc(%zu, %zu)\n",
+            filename,
+            line,
+            n,
+            size);
 
     return (void*)ptr;
 }
@@ -42,9 +62,26 @@ BST_API(void*)
 bst_debug_realloc(char *filename, size_t line, void *ptr, size_t size) {
     void *ptr2;
 
-    ptr2 = realloc(ptr, size);
+    fprintf(stderr, "BST MEM DEBUG %s:%zu begin realloc(%p, %zu)\n",
+            filename,
+            line,
+            (void*)ptr,
+            size);
 
-    fprintf(stderr, "BST MEM DEBUG %s:%zu realloc(%p, %zu) = %p\n",
+    if (!(ptr2 = bst_debug_malloc(filename, line, size))) {
+        fprintf(stderr, "BST MEM DEBUG %s:%zu end realloc(%p, %zu) = NULL\n",
+                filename,
+                line,
+                (void*)ptr,
+                size);
+
+        return NULL;
+    }
+
+    memcpy(ptr2, ptr, size);
+    bst_debug_free(filename, line, ptr);
+
+    fprintf(stderr, "BST MEM DEBUG %s:%zu end realloc(%p, %zu) = %p\n",
             filename,
             line,
             (void*)ptr,
@@ -57,9 +94,40 @@ bst_debug_realloc(char *filename, size_t line, void *ptr, size_t size) {
 
 BST_API(void)
 bst_debug_free(char *filename, size_t line, void *ptr) {
-    fprintf(stderr, "BST MEM DEBUG %s:%zu free(%p)\n",
+    uint8_t *ptr2;
+    size_t size, x;
+    char *under, *over;
+    if (!ptr) {
+        fprintf(stderr, "BST MEM DEBUG %s:%zu free(%p)\n",
+                filename,
+                line,
+                (void*)ptr);
+        return;
+    }
+
+    ptr2 = (uint8_t*)ptr - 2 * sizeof(size_t);
+    size = *(size_t*)ptr2;
+
+    under = "";
+    over = "";
+
+    memcpy(&x, (size_t*)ptr2 + 1, sizeof(size_t));
+
+    if (x != ~0)
+        under = ", LOWER BOUND VIOLATED";
+
+    memcpy(&x, (size_t*)(ptr2 + size) + 2, sizeof(size_t));
+
+    if (x != ~0)
+        over = ", UPPER BOUND VIOLATED";
+
+    fprintf(stderr, "BST MEM DEBUG %s:%zu free(%p) = %zu%s%s\n",
             filename,
             line,
-            (void*)ptr);
-    free(ptr);
+            (void*)ptr2,
+            size,
+            under,
+            over);
+
+    free(ptr2);
 }
